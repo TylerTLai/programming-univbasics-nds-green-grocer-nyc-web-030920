05d62a354e0d305659b527573c9bb1846b0529b9
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-20 21:24:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -66,7 +66,11 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[i][:cost] / coupons[i][:num],
-          
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
         }
     
     i += 1 
